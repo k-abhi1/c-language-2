4642bc2a83ef4e3c29ca0b2372d1e03a06f0a396
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-09 22:37:38 +0530

    second commit

---
+++ c language-2/function.c
@@ -0,0 +1,119 @@
+// #include<stdio.h>
+
+// int multi (int a,int b) 
+// {
+//     int c;
+//     c=a*b;
+//     return(c);
+// }
+// int div (int a,int b){
+//     int c;
+//     c=a/b;
+//     return(c);
+// }
+
+// int main()
+// {
+// int x,y;
+
+// int x=10;
+// int y=5;
+
+// int z=multi(x,y);
+// int k=div(x,y);
+
+// printf("maltiplication=%d\n",z);
+// printf("division=%d\n",k);
+
+
+
+// }
+
+
+
+// void malti(int a, int b){
+//     int c;
+//     c=a*b;
+//     return(c)
+// }
+
+// int main(){
+//  int x,y;
+//  int x=10;
+//  int x=2;
+//  int z=multi(x,y);
+//  printf("maltiplication=%d\n",z);
+// }
+
+
+
+
+// #include<stdio.h>
+//  void hello()
+//  {
+//     printf("hello wold");
+//  }
+//  void hy(){
+//     printf("hyyy");
+// }
+// int main(){
+//      hello();
+//      hy();
+// }
+
+// #include<stdio.h>
+
+// void add(int a,int b){
+//     int c;
+//     c=a+b;
+//     printf("addition=%d\n",c);
+// }
+// int main(){
+//     add(12,12);
+
+
+// }
+
+
+// #include<stdio.h>
+
+// void add()
+// {
+//     int a=1,b=3,c;
+//     c=a+b;
+//     printf("addition=%d\n",c);
+// }
+
+// int main(){
+//     add();
+
+//     return 0;
+// }
+
+
+#include<stdio.h>
+
+void add(int a,int b){
+    int c;
+    c=a+b;
+    printf("addition=%d\n",c);
+
+}
+void sub(int a,int b){
+    int c;
+    c=a-b;
+    printf("subtraction=%d\n",c);
+}
+void multi(int a,int b){
+    int c;
+    c=a*b;
+    printf("multiplication=%d\n",c);
+}
+
+int main()
+{
+    add(12,12);
+    sub(12,12);
+    multi(12,12);
+    return 0;
+}
