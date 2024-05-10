acd85fd246bafa478cbc192038fb29e84d45aaee
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-10 15:23:04 +0530

    first commit

---
+++ c language-2/array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int arry[3]={1,2,3};
+    printf("%d\n",arry[1]);
+
+}
+#include<stdio.h>
+int main()
+{
+int arry[3]={1,2,3};
+for(int i=0;i<3;i++)
+{
+    printf("%d\n",arry[i]);
+}
+
+}
