730f119af50c6b851d6348c289ab1d703e9d8b55
Abhishek Kumar <kumarabhishek7481087466@gmail.com>
2024-05-23 13:04:04 +0530

    first commit

---
+++ c language-2/array1.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+
+// int main(){
+//     int arry=[20,40,50,59,30];
+
+//    printf("array",&[1];)
+// }
+
+// hello i am abhishek in arry data structer in c language in dsa in c language 
+
+#include<stdio.h>
+
+int main (){
+    int array[]={22,23,25,26,27};
+//    printf("%d",array[1]);
+   for(int i=0; i<array[i];i++){
+    printf("%d\n",array[i]);
+   }
+}
